e86ace9cdc2c98a20cadd462bddec268e3252af1
Murphyj1098 <jrmurphy@wpi.edu>
2023-12-06 09:33:22 -0500

    Day 3: Setup

---
+++ Day3/day3.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+'''
+Advent of Code: Day 3
+'''
+
+def part1() -> int:
+    pass
+
+
+def part2() -> int:
+    pass
+
+
+if __name__ == '__main__':
+    LINES = []
+
+    LINES = open("input.txt").readlines()
+
+    print("Part 1 Answer: {}".format(part1()))
+    print("Part 2 Answer: {}".format(part2()))
